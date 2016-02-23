e41d59f476e778072dc7392c31d42173c6130bab
Narutaka Awaya <naru@pixmag.net>
2016-02-23 17:06:13 +0900

    Add room features

--- src/main.js
+++ src/main.js
@@ -1,14 +1,30 @@
-const app = require('http').createServer(handler)
-const io = require('socket.io')(app)
-const fs = require('fs')
+import http from 'http'
+import socketIO from 'socket.io'
+import _ from 'lodash'
+
+function getSingleKey(obj) {
+  return _.keys(obj)[0]
+}
+
+const app =  http.createServer()
+const io = socketIO(app)
 
 app.listen(8080)
 
-function handler(req, res) {}
+io.on('connection', (socket) => {
+  socket.on('join room', (roomId) => {
+    _(socket.rooms).forEach(roomId => {
+      socket.leave(roomId)
+    })
+    socket.join(roomId)
+  })
 
-io.on('connection', function (tocket) {
-  socket.on('post message', function (text) {
-    socket.emit('new text', {text})
-    socket.broadcast.emit('new text', {text})
+  socket.on('leave room', (roomId) => {
+    socket.leave(roomId)
+  })
+
+  socket.on('post message', (text) => {
+    const obj = {text}
+    io.to(getSingleKey(socket.rooms)).emit('new text', obj)
   })
 })
